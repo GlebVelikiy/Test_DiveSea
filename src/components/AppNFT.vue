ec51ed144c70142ad66175caf90abb4526044df0
Gleb Velikiy <glebvelikij8@gmail.com>
2025-03-23 13:02:30 +0300

    Add Timer

--- src/components/AppNFT.vue
+++ src/components/AppNFT.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="nft__container">
+        <div class="nft__timer">{{ countdown }}</div>
         <img :src="item.image" alt="" class="nft__img">
         <h3 class="nft__heading">
             {{ item.name }}
@@ -22,9 +23,48 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, ref, onMounted, onUnmounted } from 'vue';
 
-defineProps(['item'])
+const props = defineProps(['item']);
+
+const countdown = ref('07h 09m 12s');
+
+let timer: number | undefined;
+
+onMounted(() => {
+  startCountdown();
+});
+
+onUnmounted(() => {
+  clearInterval(timer);
+});
+
+function randomTime() {
+  return Math.floor(Math.random() * (12000 - 3000 + 1)) + 3000;
+}
+
+function startCountdown() {
+  let timeLeft = randomTime();
+
+  timer = setInterval(() => {
+    if (timeLeft > 0) {
+      timeLeft--;
+      updateCountdown(timeLeft);
+    }
+  }, 1000);
+}
+
+function updateCountdown(seconds: number) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  countdown.value = `${padZero(hours)}h ${padZero(minutes)}m ${padZero(remainingSeconds)}s`;
+}
+
+function padZero(num: number): string {
+  return num.toString().padStart(2, '0');
+}
 </script>
 
 <style scoped lang="scss">
